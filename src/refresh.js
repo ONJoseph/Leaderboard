ed9d1596ba510dbdd9f8a05b2ed2047b6cbdc54c
ON Joseph <on.josephinfo@gmail.com>
2022-05-11 22:32:53 +0100

    fix typo in refresh.js

--- src/refresh.js
+++ src/refresh.js
@@ -2,7 +2,7 @@ import Result from './result.js';
 
 export default async function refresh() {
   const list = document.querySelector('.results');
-  list.styles.border = '2px solid black';
+  list.style.border = '2px solid black';
   list.innerHTML = '';
   const results = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jT5ddO4HfUyAH1uOfTZ3/scores/');
   const data = await results.json();
